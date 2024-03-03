075dd5c5bec8e354fa99a53ec8c901760fb62992
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-03 17:51:35 +0100

    init

---
+++ Source/SecondProjectCPP/SecondProjectCPP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SecondProjectCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecondProjectCPP, "SecondProjectCPP" );
